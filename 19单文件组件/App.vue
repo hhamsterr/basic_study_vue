63dce1501ac7c617d2982d7997e5d519be759f66
hhamsterr <857556016@qq.com>
2021-07-15 20:55:17 +0800

    first commit

---
+++ 19单文件组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <School/>
+    </div>
+</template>
+<script>
+//引入school组件
+import School from './components/School.vue'
+
+export default {
+   components:{
+       School
+   } 
+}
+</script>
+<style scoped>
+
+</style>
---
+++ 19单文件组件/components/School.vue
@@ -0,0 +1,18 @@
+/* 配置模板结构*/
+<template>
+    <div>
+        <h2>学校名：{{name}}</h2>
+    </div>
+</template>
+
+/* 配置组件数据、交互、事件等*/
+<script>
+//暴露组件的配置，没有创建组件，因为没有调用vue.extend
+export default{
+    data(){
+        return {
+            name:'chd'
+        }
+    }
+}
+</script>
